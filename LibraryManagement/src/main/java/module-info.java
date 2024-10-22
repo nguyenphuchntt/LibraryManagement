7f5babdde303325d0163f30ac895cab09eee83d8
nguyenphuchntt <nguyenvanphucuet@gmail.com>
2024-10-22 13:39:26 +0700

    initial commit - add project directory

---
+++ LibraryManagement/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module uet.oop.librarymanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+//    opens uet.oop.librarymanagement to javafx.fxml;
+//    exports uet.oop.librarymanagement;
+}
