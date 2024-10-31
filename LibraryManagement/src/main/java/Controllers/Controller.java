cf190ea5c1434c6f3f9d2c8f6c38ab578cfed9fd
nguyenphuchntt <nguyenvanphucuet@gmail.com>
2024-10-31 17:03:26 +0700

    update[sidebar]: lazy scene loading, setSideBar function

--- LibraryManagement/src/main/java/Controllers/Controller.java
+++ LibraryManagement/src/main/java/Controllers/Controller.java
@@ -9,18 +9,21 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controller {
 
-    private Stage stage;
+    private static Stage stage;
+    private Map<String, Scene> scenes = new HashMap<>();
 
     private FXMLLoader loadFxml(String fxmlFileName) throws IOException {
         return new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName));
     }
 
 
-    public void setStage(Stage stage) {
-        this.stage = stage;
+    public static void setStage(Stage stage) {
+        Controller.stage = stage;
     }
 
     @FXML
@@ -69,19 +72,33 @@ public class Controller {
     }
 
     private void switchScene(String fxmlFileName) throws Exception {
-        AnchorPane newRoot = null;
-        try {
-            newRoot = (AnchorPane) loadFxml(fxmlFileName).load();
-        } catch (Exception e) {
-            System.out.println("Error loading fxml: " + fxmlFileName);
-            throw e;
+        Scene newScene;
+        if (scenes.get(fxmlFileName) != null) {
+            AnchorPane root = (AnchorPane) scenes.get(fxmlFileName).getRoot();
+            newScene = scenes.get(fxmlFileName);
+            if (!root.getChildren().contains(SideBarLoader.getLeftSidebar())) {
+                Controller.setSideBar(root);
+            }
+        } else {
+            AnchorPane newRoot = null;
+            try {
+                newRoot = (AnchorPane) loadFxml(fxmlFileName).load();
+            } catch (Exception e) {
+                System.out.println("Error loading fxml: " + fxmlFileName);
+                throw e;
+            }
+            newScene = new Scene(newRoot);
+            Controller.setSideBar(newRoot);
+            scenes.put(fxmlFileName, newScene);
         }
 
-        Scene newScene = new Scene(newRoot);
+        stage.setScene(newScene);
+        stage.show();
+    }
 
+    public static void setSideBar(AnchorPane root) throws IOException {
         HBox topSidebar = SideBarLoader.getTopSidebar();
         VBox leftSidebar = SideBarLoader.getLeftSidebar();
-        SideBarLoader.getController().setStage(stage); // can be deleted
 
         AnchorPane.setTopAnchor(topSidebar, 0.0);
         AnchorPane.setLeftAnchor(topSidebar, 0.0);
@@ -89,10 +106,7 @@ public class Controller {
         AnchorPane.setTopAnchor(leftSidebar, 45.0);
         AnchorPane.setLeftAnchor(leftSidebar, 0.0);
 
-        newRoot.getChildren().add(topSidebar);
-        newRoot.getChildren().add(leftSidebar);
-
-        stage.setScene(newScene);
-        stage.show();
+        root.getChildren().add(topSidebar);
+        root.getChildren().add(leftSidebar);
     }
 }
