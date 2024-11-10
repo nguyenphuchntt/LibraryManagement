3041d4c541e745b0c9e3a1a60cdc248ba8af44f3
nguyenphuchntt <nguyenvanphucuet@gmail.com>
2024-11-10 15:50:29 +0700

    add[controller]: login/signup

--- LibraryManagement/src/main/java/Controllers/LoginController.java
+++ LibraryManagement/src/main/java/Controllers/LoginController.java
@@ -1,4 +1,77 @@
 package Controllers;
 
+import Entity.Account;
+import Entity.Library;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
 public class LoginController {
+
+    @FXML
+    private TextField username_TextField;
+    @FXML
+    private TextField password_TextField; // change to password field
+    @FXML
+    private Label loginMessage_Label;
+
+    private List<Account> accounts;
+
+    @FXML
+    public void initialize() {
+        accounts = Library.getInstance().getManagerAccounts(); // chi lay manager account
+
+        accounts.add(new Account.Builder()
+                .username("admin001")
+                .password("Admin001")
+                .build()
+        );           // account for testing
+    }
+
+    @FXML
+    private void loginButtonOnAction(ActionEvent event) {
+        String username = username_TextField.getText();
+        String password = password_TextField.getText();
+
+//        if (accounts.login(username, password)) {
+//            loginMessage_Label.setText("Đăng nhập thành công!");
+//            switchToHomeScene();
+//        } else {
+//            loginMessage_Label.setText("Tên đăng nhập hoặc mật khẩu không đúng.");
+//        }
+        for (Account account : accounts) {
+            if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
+                loginMessage_Label.setText("Welcome " + account.getUsername());
+                switchToHomeScene();
+            }
+        }
+        loginMessage_Label.setText("Invalid username or password");
+    }
+
+    @FXML
+    private void handleDontHaveAccount(ActionEvent event) {
+        switchToSignupScene();
+    }
+
+    private void switchToSignupScene() {
+        try {
+            Controller.getInstance().goToScene("Signup.fxml");
+        } catch (Exception e) {
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
+    private void switchToHomeScene() {
+        try {
+            Controller.getInstance().goToScene("Home.fxml");
+        } catch (Exception e) {
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
 }
--- LibraryManagement/src/main/java/Controllers/SignupController.java
+++ LibraryManagement/src/main/java/Controllers/SignupController.java
@@ -1,4 +1,63 @@
 package Controllers;
 
+import Entity.Account;
+import Entity.Library;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
 public class SignupController {
+
+    private List<Account> accounts;
+
+    @FXML
+    private TextField username_TextField;
+    @FXML
+    private TextField password_TextField; // change to password field
+    @FXML
+    private TextField confirmPassword_TextField; // change to password field
+
+    @FXML
+    public void initialize() {
+        accounts = Library.getInstance().getManagerAccounts();
+    }
+
+    @FXML
+    private void handleCreateAccount(ActionEvent event) {
+        String username = username_TextField.getText();
+        String password = password_TextField.getText();
+        String confirmPassword = confirmPassword_TextField.getText();
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            throw new IllegalArgumentException("All fields are required");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new IllegalArgumentException("Passwords do not match");
+        }
+        Account newAccount = new Account.Builder()
+                .username(username)
+                .password(password)
+                .typeAccount("user")
+                .build();
+        accounts.add(newAccount);
+
+        switchToLoginScene();
+    }
+
+    @FXML
+    private void handleHadAccount(ActionEvent event) {
+        switchToLoginScene();
+    }
+
+    private void switchToLoginScene() {
+        try {
+            Controller.getInstance().goToScene("Login.fxml");
+        } catch (Exception e) {
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
+
 }
